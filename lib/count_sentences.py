c49543bc38f8862a0719a53f506178aeec738ba1
leroy <leroy.kariuki@student.moringaschool.com>
2023-12-09 13:33:27 +0300

    done

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,21 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        if not isinstance(value, str):
+            raise ValueError("The value must be a string.")
+        else:
+            self.value = value
+
+    def is_sentence(self):
+        return self.value.endswith('.')
+
+    def is_question(self):
+        return self.value.endswith('?')
+
+    def is_exclamation(self):
+        return self.value.endswith('!')
+
+    def count_sentences(self):
+        sentences = [sentence for sentence in self.value.split('.') if sentence]
+        sentences += [sentence for sentence in self.value.split('!') if sentence]
+        sentences += [sentence for sentence in self.value.split('?') if sentence]
+        return len(sentences)
--- lib/testing/count_sentences_test.py
+++ lib/testing/count_sentences_test.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python3
-
-from count_sentences import MyString
-
 import io
 import sys
+import pytest
+from count_sentences import MyString
 
 class TestMyString:
     '''MyString in count_sentences.py'''
@@ -11,37 +9,33 @@ class TestMyString:
     def test_is_class(self):
         '''is a class with the name "MyString".'''
         string = MyString()
-        assert(type(string) == MyString)
+        assert type(string) == MyString
 
     def test_value_string(self):
-        '''prints "The value must be a string." if not string.'''
-        captured_out = io.StringIO()
-        sys.stdout = captured_out
-        string = MyString()
-        string.value = 123
-        sys.stdout = sys.__stdout__
-        assert(captured_out.getvalue() == "The value must be a string.\n")
+        '''raises a ValueError with the message "The value must be a string." if not string.'''
+        with pytest.raises(ValueError, match="The value must be a string.") as exc_info:
+            string = MyString(value=123)
 
     def test_is_sentence(self):
         '''returns True if value ends with a period and False otherwise.'''
-        assert(MyString("Hello World.").is_sentence() == True)
-        assert(MyString("Hello World").is_sentence() == False)
+        assert MyString("Hello World.").is_sentence() == True
+        assert MyString("Hello World").is_sentence() == False
 
     def test_is_question(self):
         '''returns True if value ends with a question mark and False otherwise.'''
-        assert(MyString("Is anybody there?").is_question() == True)
-        assert(MyString("Is anybody there").is_question() == False)
+        assert MyString("Is anybody there?").is_question() == True
+        assert MyString("Is anybody there").is_question() == False
 
     def test_is_exclamation(self):
         '''returns True if value ends with an exclamation mark and False otherwise.'''
-        assert(MyString("It's me!").is_exclamation() == True)
-        assert(MyString("It's me").is_exclamation() == False)
+        assert MyString("It's me!").is_exclamation() == True
+        assert MyString("It's me").is_exclamation() == False
 
     def test_count_sentences(self):
         '''returns the number of sentences in the value.'''
         simple_string = MyString("one. two. three?")
         empty_string = MyString()
         complex_string = MyString("This, well, is a sentence. This is too!! And so is this, I think? Woo...")
-        assert(simple_string.count_sentences() == 3)
-        assert(empty_string.count_sentences() == 0)
-        assert(complex_string.count_sentences() == 4)
+        assert simple_string.count_sentences() == 3
+        assert empty_string.count_sentences() == 0
+        assert complex_string.count_sentences() == 4
